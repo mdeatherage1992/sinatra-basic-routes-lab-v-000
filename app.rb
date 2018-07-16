90218c0a5304243fe9e41d18fe4d8666ec8e489c
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-16 18:35:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+  "Hello, World!"
+end
+get '/hometown' do
+  "Hello, World!"
+end
+get '/favorite-food' do
+  "Hello, World!"
+end
 end
